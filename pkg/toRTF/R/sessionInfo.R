c81deca65295f9d838f9656bac0303cc8bd3c5d5
weigand <weigand@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-03-28 14:20:19 +0000

    Update and cleanup
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rtfsweave@95 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/toRTF/R/sessionInfo.R
+++ pkg/toRTF/R/sessionInfo.R
@@ -4,49 +4,56 @@
 ## Started March 27, 2016
 ##
 ## * TODO: Enhancements
-## - [ ] Pull from rtf.options
+## - [ ] Eventually pull from rtf.options
 ## - [ ] See what pander.sessionInfo does
 
 toRTF.sessionInfo <- function (object, locale = TRUE, ...) {
+    decorate <- function(x, commands = "\\i"){
+        paste0("{", commands, " ", x, "}")
+    }
     opkgver <- sapply(object$otherPkgs, function(x) x$Version)
     nspkgver <- sapply(object$loadedOnly, function(x) x$Version)
     z <- c("{\\pard \\f0",
-           paste0("{\\b  ", 
+           paste0("{\\b ", 
                   object$R.version$version.string,
-                  "}, ",
-                  "{\f2 ",
+                  "} on ",
+                  "{\\f2 ",
                   object$R.version$platform,
-                  "}"),
+                  "}; running under ",
+                  object$running),
            "\\par}")
     if (locale) {
         z <- c(z,
                "{\\pard \\f0 ",
                paste0("{\\f2 ",
-                      gsub(";", "},\n{\\\\f2 ", object$locale),
+                      gsub(";", "},\n {\\\\f2 ", object$locale),
                       "}"),
                "\\par}")
     }
     z <- c(z,
-           "{\\pard \\f0 {\\b Base packages:} {\\i ",
-           paste0(sort(object$basePkgs), collapse = ", "),
-           "}\\par}")
+           "{\\pard \\f0 {\\b Base packages:}",
+           paste0(decorate(sort(object$basePkgs), "\\i"),
+                  collapse = ", "),
+           "\\par}")
 
     if (length(opkgver)) {
         opkgver <- opkgver[sort(names(opkgver))]
         z <- c(z,
-               "{\\pard \\f0 {\\b Other packages:} {\\i ",
-               paste(names(opkgver), opkgver, sep = "\\~", collapse = ", "),
-               "}\\par}")
+               "{\\pard \\f0 {\\b Other packages:}",
+               paste(decorate(names(opkgver), "\\i"), opkgver, sep = "\\~v",
+                     collapse = ", "),
+               "\\par}")
                        
     }
     if (length(nspkgver)) {
         nspkgver <- nspkgver[sort(names(nspkgver))]
         z <- c(z,
-               "{\\pard \\f0 {\\b Loaded via a namespace (and not attached):} {\\i ",
-               paste(names(nspkgver), nspkgver, sep = "~", collapse = ", "),
-               "}\\par}")
+               "{\\pard \\f0 {\\b Loaded via a namespace (and not attached):}",
+               paste(decorate(names(nspkgver), "\\i"), nspkgver, sep = "\\~v",
+                     collapse = ", "),
+               "\\par}")
     }
-    z <- c(z, "\\end{itemize}")
     class(z) <- "RTF"
     z
 }
+
