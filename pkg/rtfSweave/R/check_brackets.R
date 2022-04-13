ee1dadc8ebf7268ef61404f5159f2833535bcd70
weigand (Mr. ) <weigand.stephen@gmail.com>
2022-04-13 03:29:44 +0000

    Some check_brackets.{R,Rd} work
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rtfsweave@139 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/rtfSweave/R/check_brackets.R
+++ pkg/rtfSweave/R/check_brackets.R
@@ -2,6 +2,5 @@ check_brackets <- function(filename) {
     if (length(filename) > 1L) {
         message("Only checking first filename given")
     }
-    out <- .C(check_rtf_brackets, as.character(filename))
-    invisible(NULL)
+    .C(check_rtf_brackets, as.character(filename))
 }    
