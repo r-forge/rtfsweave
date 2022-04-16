82f279176c3c39989322f4cfea762e45d51b10f3
weigand (Mr. ) <weigand.stephen@gmail.com>
2022-04-16 20:01:59 +0000

    Hoping I fixed a bug in calling the compiled code. Added documentation to the status codes 'check_brackets' returns.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rtfsweave@140 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/rtfSweave/R/check_brackets.R
+++ pkg/rtfSweave/R/check_brackets.R
@@ -1,6 +1,7 @@
 check_brackets <- function(filename) {
-    if (length(filename) > 1L) {
-        message("Only checking first filename given")
+    if (!(length(filename) == 1L & is.character(filename))) {
+        stop(sQuote("filename"), " must be a length-one character vector")
     }
-    .C(check_rtf_brackets, as.character(filename))
+    out <- .C(check_rtf_brackets, as.character(filename), 99L)
+    out$status_code
 }    
